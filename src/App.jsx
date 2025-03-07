1bdf6472d0826e1e11be7f60ad5530df408c3bbb
Jaswinder Singh <32540681+jaswindersingh1903@users.noreply.github.com>
2025-03-06 21:21:10 -0500

    added some loren text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import './App.css'
-import bannerImage from "./assets/logo.png"; // Import the banner image
+import bannerImage from "./assets/logo.png";
 import Survey from "./Survey";
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
             <img src={bannerImage} alt="Survey Banner" />
           </header>
           <h1>Welcome to the Survey</h1>
+          <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa repudiandae, quaerat suscipit magni nisi optio aliquid doloremque quidem et reiciendis fuga cupiditate numquam, quibusdam nam expedita esse? Laboriosam, sunt eum.</h6>
           <button onClick={() => setSurveyStarted(true)}>Start Survey</button>
         </div>
       ) : (
